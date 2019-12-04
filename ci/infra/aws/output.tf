643d27791b8fc23dc5450c7023f7ef4a9fbb9104
Flavio Castelli <fcastelli@suse.com>
2019-12-04 17:18:28 +0100

    AWS deployment: move to terraform 0.12
    Ensure the AWS deployment can be done with terraform 0.12
    
    Signed-off-by: Flavio Castelli <fcastelli@suse.com>

--- ci/infra/aws/output.tf
+++ ci/infra/aws/output.tf
@@ -1,11 +1,12 @@
-output "control_plane.public_ip" {
-  value = "${aws_instance.control_plane.*.public_ip}"
+output "control_plane_public_ip" {
+  value = aws_instance.control_plane.*.public_ip
 }
 
-output "control_plane.private_dns" {
-  value = "${aws_instance.control_plane.*.private_dns}"
+output "control_plane_private_dns" {
+  value = aws_instance.control_plane.*.private_dns
 }
 
-output "nodes.private_dns" {
-  value = "${aws_instance.nodes.*.private_dns}"
+output "nodes_private_dns" {
+  value = aws_instance.nodes.*.private_dns
 }
+
